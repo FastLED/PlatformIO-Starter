0283d278b49008409f878a38b8be81d6eab907e2
Zachary Vorhies <zachvorhies@protonmail.com>
2024-12-23 13:42:29 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
-void setup() {
-
+void wait_for_serial_connection() {
   uint32_t timeout_end = millis() + 2000;
   Serial.begin(115200);
   while(!Serial && timeout_end < millis()) {}  //wait until the connection to the PC is established
-
   
+}
+
+void setup() {
+  wait_for_serial_connection(); // Optional, but seems to help Teensy out a lot.
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
 }
